87928d3c4c6938fe69c3a3300ce3ce19834185fb
Samat Kalshabekov <skalshabekov@beeline.kz>
2022-06-11 17:13:07 +0600

    completing homework template

--- src/App.js
+++ src/App.js
@@ -1,28 +1,115 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
 // button-group
 const buttons = [
   {
-    name: "all",
+    type: "all",
     label: "All",
   },
   {
-    name: "active",
+    type: "active",
     label: "Active",
   },
   {
-    name: "done",
+    type: "done",
     label: "Done",
   },
 ];
 
+const toDoItems = [
+  {
+    key: uuidv4(),
+    label: "Have fun",
+  },
+  {
+    key: uuidv4(),
+    label: "Spread Empathy",
+  },
+  {
+    key: uuidv4(),
+    label: "Generate Value",
+  },
+];
+
+// helpful links:
+// useState crash => https://blog.logrocket.com/a-guide-to-usestate-in-react-ecb9952e406c/
 function App() {
+  const [itemToAdd, setItemToAdd] = useState("");
+  //arrow declaration => expensive computation ex: API calls
+  const [items, setItems] = useState(() => toDoItems);
+
+  const [filterType, setFilterType] = useState("");
+
+  const handleChangeItem = (event) => {
+    setItemToAdd(event.target.value);
+  };
+
+  const handleAddItem = () => {
+    // mutating !WRONG!
+    // const oldItems = items;
+    // oldItems.push({ label: itemToAdd, key: uuidv4() });
+    // setItems(oldItems);
+
+    // not mutating !CORRECT!
+    setItems((prevItems) => [
+      { label: itemToAdd, key: uuidv4() },
+      ...prevItems,
+    ]);
+
+    setItemToAdd("");
+  };
+
+  const handleItemDone = ({ key }) => {
+    //first way
+    // const itemIndex = items.findIndex((item) => item.key === key);
+    // const oldItem = items[itemIndex];
+    // const newItem = { ...oldItem, done: !oldItem.done };
+    // const leftSideOfAnArray = items.slice(0, itemIndex);
+    // const rightSideOfAnArray = items.slice(itemIndex + 1, items.length);
+    // setItems([...leftSideOfAnArray, newItem, ...rightSideOfAnArray]);
+
+    //  second way
+    // const changedItem = items.map((item) => {
+    //   if (item.key === key) {
+    //     return { ...item, done: item.done ? false : true };
+    //   } else return item;
+    // });
+
+    //second way updated
+    setItems((prevItems) =>
+      prevItems.map((item) => {
+        if (item.key === key) {
+          return { ...item, done: !item.done };
+        } else return item;
+      })
+    );
+  };
+
+  const handleFilterItems = (type) => {
+    setFilterType(type);
+  };
+
+  const amountDone = items.filter((item) => item.done).length;
+
+  const amountLeft = items.length - amountDone;
+
+  const filteredItems =
+    !filterType || filterType === "all"
+      ? items
+      : filterType === "active"
+      ? items.filter((item) => !item.done)
+      : items.filter((item) => item.done);
+
   return (
     <div className="todo-app">
       {/* App-header */}
       <div className="app-header d-flex">
         <h1>Todo List</h1>
-        <h2>5 more to do, 3 done</h2>
+        <h2>
+          {amountLeft} more to do, {amountDone} done
+        </h2>
       </div>
 
       <div className="top-panel d-flex">
@@ -34,73 +121,65 @@ function App() {
         />
         {/* Item-status-filter */}
         <div className="btn-group">
-          {/* Button group */}
-          <button type="button" className="btn btn-info">
-            All
-          </button>
-          <button type="button" className="btn btn-info btn-outline-secondary">
-            Active
-          </button>
-          <button type="button" className="btn btn-info btn-outline-secondary">
-            Done
-          </button>
+          {buttons.map((item) => (
+            <button
+              onClick={() => handleFilterItems(item.type)}
+              key={item.type}
+              type="button"
+              className={`btn btn-${
+                filterType !== item.type ? "outline-" : ""
+              }info`}
+            >
+              {item.label}
+            </button>
+          ))}
         </div>
       </div>
 
       {/* List-group */}
       <ul className="list-group todo-list">
-        {/* simple item */}
-        <li className="list-group-item">
-          <span className="todo-list-item">
-            <span className="todo-list-item-label">first Item</span>
+        {filteredItems.length > 0 &&
+          filteredItems.map((item) => (
+            <li key={item.key} className="list-group-item">
+              <span className={`todo-list-item${item.done ? " done" : ""}`}>
+                <span
+                  className="todo-list-item-label"
+                  onClick={() => handleItemDone(item)}
+                >
+                  {item.label}
+                </span>
 
-            <button
-              type="button"
-              className="btn btn-outline-success btn-sm float-right"
-            >
-              <i className="fa fa-exclamation" />
-            </button>
+                <button
+                  type="button"
+                  className="btn btn-outline-success btn-sm float-right"
+                >
+                  <i className="fa fa-exclamation" />
+                </button>
 
-            <button
-              type="button"
-              className="btn btn-outline-danger btn-sm float-right"
-            >
-              <i className="fa fa-trash-o" />
-            </button>
-          </span>
-        </li>
-
-        {/* Important item */}
-        <li className="list-group-item">
-          <span className="todo-list-item important">
-            <span className="todo-list-item-label">Important Item</span>
-
-            <button
-              type="button"
-              className="btn btn-outline-success btn-sm float-right"
-            >
-              <i className="fa fa-exclamation" />
-            </button>
-
-            <button
-              type="button"
-              className="btn btn-outline-danger btn-sm float-right"
-            >
-              <i className="fa fa-trash-o" />
-            </button>
-          </span>
-        </li>
+                <button
+                  type="button"
+                  className="btn btn-outline-danger btn-sm float-right"
+                >
+                  <i className="fa fa-trash-o" />
+                </button>
+              </span>
+            </li>
+          ))}
       </ul>
 
       {/* Add form */}
-      <form className="item-add-form d-flex">
+      <div className="item-add-form d-flex">
         <input
+          value={itemToAdd}
           type="text"
           className="form-control"
           placeholder="What needs to be done"
+          onChange={handleChangeItem}
         />
-        <button className="btn btn-outline-secondary">Add item</button>
-      </form>
+        <button className="btn btn-outline-secondary" onClick={handleAddItem}>
+          Add item
+        </button>
+      </div>
     </div>
   );
 }
